5f8f4a5869e8ecf47bb28e10675429692618c92b
Marcos Ramos <rodrigues.ramos@gmail.com>
2024-03-20 21:00:19 -0300

    update User.java

--- users/src/main/java/br/com/it/users/domain/model/User.java
+++ users/src/main/java/br/com/it/users/domain/model/User.java
@@ -1,5 +1,14 @@
 package br.com.it.users.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }

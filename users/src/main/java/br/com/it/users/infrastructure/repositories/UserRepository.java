a01d8d89c29ce2ede13f4e6cc36835d1837abb5f
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-03-26 18:20:45 -0300

    .

--- users/src/main/java/br/com/it/users/infrastructure/repositories/UserRepository.java
+++ users/src/main/java/br/com/it/users/infrastructure/repositories/UserRepository.java
@@ -4,5 +4,5 @@ import br.com.it.users.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
 }

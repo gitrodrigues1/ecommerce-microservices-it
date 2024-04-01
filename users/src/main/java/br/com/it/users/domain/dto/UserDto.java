1029605748444f452d1097f1ac585c33a29a10f1
Joseane Branco <joseane.pereira@infocaltechnologi.com>
2024-04-01 19:37:56 -0300

    Add deleteById

--- users/src/main/java/br/com/it/users/domain/dto/UserDto.java
+++ users/src/main/java/br/com/it/users/domain/dto/UserDto.java
@@ -1,10 +1,13 @@
 package br.com.it.users.domain.dto;
 
+import br.com.it.users.domain.model.UserEnum;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import br.com.it.users.domain.model.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+
+import java.util.Locale;
 
 public class UserDto {
     
@@ -17,7 +20,16 @@ public class UserDto {
     @NotNull
     @Email
     private String email;
-    
+
+    @NotNull
+    private String dataNascimento;
+
+    @NotNull
+    private UserEnum userEnum;
+
+    @NotNull
+    private String senha;
+
     public UserDto() {
     }
     
@@ -25,10 +37,14 @@ public class UserDto {
         this.id = user.getId();
         this.name = user.getName();
         this.email = user.getEmail();
+        this.senha = user.getSenha();
     }
-    public UserDto(String name, String email) {
+    public UserDto(String name, String email, String dataNascimento, String senha, UserEnum userEnum) {
         this.name = name;
         this.email = email;
+        this.senha = senha;
+        this.userEnum = userEnum;
+        this.dataNascimento = dataNascimento;
     }
 
     public String getName() {
@@ -44,10 +60,19 @@ public class UserDto {
         this.email = email;
     }
 
+    public String getDataNascimento() { return dataNascimento; }
+
+    public void setDataNascimento(String dataNascimento) { this.dataNascimento = dataNascimento; }
+
+    public String getSenha() { return senha; }
+
+    public void setSenha(String senha) { this.senha = senha; }
+
     public User toModel() {
         var user = new User();
         user.setName(this.name);
         user.setEmail(this.email);
+        user.setSenha(this.senha);
 
         return user;
     }
@@ -58,5 +83,11 @@ public class UserDto {
         this.id = id;
     }
 
-    
+    public UserEnum getUserEnum() {
+        return userEnum;
+    }
+
+    public void setUserEnum(UserEnum userEnum) {
+        this.userEnum = userEnum;
+    }
 }
--- users/src/main/java/br/com/it/users/domain/model/User.java
+++ users/src/main/java/br/com/it/users/domain/model/User.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "tb_user")
 public class User {
@@ -16,14 +18,20 @@ public class User {
 
     private String name;
     private String email;
+    private LocalDate dataNascimento;
+    private String senha;
+    private UserEnum userEnum;
 
     public User() {
     }
 
-    public User(Long id, String name, String email) {
+    public User(Long id, String name, String email, LocalDate dataNascimento, String senha) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.senha = senha;
+
     }
 
     public Long getId() {
@@ -48,5 +56,24 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+    public String getSenha() { return senha; }
+
+    public void setSenha(String senha) { this.senha = senha; }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public UserEnum getUserEnum() {
+        return userEnum;
+    }
+
+    public void setUserEnum(UserEnum userEnum) {
+        this.userEnum = userEnum;
     }
 }
